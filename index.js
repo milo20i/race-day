ba5cac1d0ebea2445849dae1bf9d5df2c2f93f79
Danilo De Matteis <62311664+milo20i@users.noreply.github.com>
2021-11-16 12:16:01 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+
+let registeredEarly = true;
+
+let runnerAge = 20;
+
+if (runnerAge > 18 && registeredEarly) {
+   raceNumber += 1000;
+}
+
+if (runnerAge > 18 && registeredEarly) {
+  console.log(`You will race at 9:30 am and your race number is ${raceNumber}`);
+} else if (runnerAge > 18 && !registeredEarly) {
+  console.log(`You will race at 11:00 am and your race number is ${raceNumber}`);
+} else if (runnerAge < 18) {
+  console.log(`You will rune at 12:30 pm and your race number is ${raceNumber}`);
+} else {
+  console.log ('Please see registration desk');
+};
